bf75b7036570864c16fa8b852b2ffadf21da191a
Udaykiran87 <uday.patnaik@gmail.com>
2022-06-25 21:45:25 +0900

    initial setup done

--- init_setup.sh
+++ init_setup.sh
@@ -1,16 +1,22 @@
 echo [$(date)]: "START"
 echo [$(date)]: "creating environment"
-conda create --prefix ./env python=3.7 -y
+conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activate environment"
 source activate ./env
+echo [$(date)]: "create folder structure"
+
+for DIR in data_ingestion data_preparation data_validation model model_eval model_trainer
+do
+    echo "Creating", "src/NER_"$DIR
+    mkdir -p "src/NER_"$DIR
+    echo "Creating __init__.py inside above folders"
+    touch "src/NER_"$DIR/"__init__.py" "src/NER_"$DIR/$DIR".py"
+    echo "from src.NER_"$DIR"."$DIR" import *" > "src/NER_"$DIR/"__init__.py"
+done
 echo [$(date)]: "install requirements"
 pip install -r requirements.txt
-echo [$(date)]: "export conda environment"
-conda env export > conda.yaml
-echo "# ${PWD}" > README.md
-echo [$(date)]: "first commit"
-git add .
-git commit -m "first commit"
+echo [$(date)]: "install torch dependency"
+pip install torch --extra-index-url https://download.pytorch.org/whl/cu113 -q
 echo [$(date)]: "END"
 
 # to remove everything -
